368c957209f3cb7610caef3dea2d79e9b7c61dc9
root <1505243015@qq.com>
2021-10-10 21:53:25 +0800

    update 10.10

---
+++ 微软/codetop常见面试题/22.cpp
@@ -0,0 +1,29 @@
+#include  <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    void backtrack(vector<string>& ans, string& cur, int open, int close, int n){
+        if (cur.size() == n*2){
+            ans.push_back(cur);
+            return;
+        }
+        if (open<n)
+        {
+            cur.push_back('(');
+            backtrack(ans,cur,open+1,close,n);
+            cur.pop_back();
+        }
+          if (close < open) {
+            cur.push_back(')');
+            backtrack(ans, cur, open, close + 1, n);
+            cur.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string current;
+        backtrack(result,current,0,0,n);
+        return result;
+    }
+};
