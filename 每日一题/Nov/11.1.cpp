afb6e2903bb685acbb08447a88d23a7f5a9a560c
root <1505243015@qq.com>
2021-11-02 22:35:56 +0800

    update 11.12

---
+++ 每日一题/Nov/11.1.cpp
@@ -0,0 +1,9 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int candy_num = candyType.size();
+        
+    }
+};
