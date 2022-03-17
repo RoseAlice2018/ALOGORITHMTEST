3bac2fdc6a760c92390d8c45575365b7896a8323
root <1505243015@qq.com>
2022-03-17 20:16:52 +0800

    update 3.17

---
+++ 每日一题/Mar/3.12.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+using namespace std;
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> ret;
+    vector<int> postorder(Node* root) {
+        if(root==nullptr)
+        {
+            return ret;
+        }
+        else
+        {
+            for(int i=0;i<root->children.size();i++)
+            {
+                postorder(root->children[i]);
+            }
+            ret.push_back(root->val);
+        }
+        return ret;
+    }
+};
---
+++ 每日一题/Mar/3.17.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+class Solution{
+    public:
+        string longestWord(vector<string> &words)
+        {
+            sort(words.begin(),words.end(),[](const string& a,const string& b){
+                return a.size()!=b.size()?a.size()<b.size():a>b;
+            });
+            string longest;
+            unordered_set<string> candidates = {""};
+            for(const auto& word : words)
+            {
+                if(candidates.count(word.substr(0,word.size()-1)))
+                {
+                    candidates.emplace(word);
+                    longest = word;
+                }
+            }
+            return longest;
+        }
+};
