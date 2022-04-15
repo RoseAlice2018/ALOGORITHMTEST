94630bd7743149b3150e58f728b1ed6b518c76c9
root <1505243015@qq.com>
2022-04-15 18:26:50 +0800

    update 4.15

---
+++ 每日一题/Apr/4.10.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include<string>
+using namespace std;
+const static string MORSE[] = {
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+        "....", "..", ".---", "-.-", ".-..", "--", "-.",
+        "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+        "...-", ".--", "-..-", "-.--", "--.."
+};
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        unordered_set<string> seen;
+        for(auto& word: words){
+            string code;
+            for(auto& c: word){
+                code.append(MORSE[c-'a']);
+            }
+            seen.emplace(code);
+        }
+        return seen.size();
+    }
+};
