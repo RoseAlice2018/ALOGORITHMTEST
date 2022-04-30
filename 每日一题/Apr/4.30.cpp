f078614286f0135ecaa6e83ddd9f6352bdc967fc
root <1505243015@qq.com>
2022-04-30 16:50:46 +0800

    update 4.30

---
+++ 每日一题/Apr/4.30.cpp
@@ -0,0 +1,10 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {
+        int minNum = *min_element(nums.begin(),nums.end());
+        int maxNum = *max_element(nums.begin(),nums.end());
+        return max(0,maxNum-minNum-2*k);              
+    }
+};
