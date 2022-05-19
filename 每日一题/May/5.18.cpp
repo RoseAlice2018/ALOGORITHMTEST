b0b8dcb48f6986ce0441563cf1adcd8e7905f5a3
root <1505243015@qq.com>
2022-05-19 17:05:19 +0800

    update 5.19

---
+++ 每日一题/May/5.18.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size(),ret = 0,x = nums[n / 2];
+        for(int i=0;i<n;i++)
+        {
+            ret += abs(nums[i] - x);
+        }
+        return ret;
+    }
+};
