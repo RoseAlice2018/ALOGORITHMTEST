c60804c4e9c0acaca1fa7f967f50d666e927d2af
root <1505243015@qq.com>
2022-07-11 16:22:30 +0800

    update 7.11

---
+++ 每日一题/July/7.11.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class MagicDictionary {
+public:
+    MagicDictionary() {
+        
+    }
+    void buildDict(vector<string> dictionary) {
+        words = dictionary;
+    }
+    
+    bool search(string searchWord) {
+        for(auto&& word : words)
+        {
+            if(word.size() != searchWord.size()){
+                continue;
+            }
+
+            int diff = 0;
+            for(int i = 0; i < word.size(); ++i)
+            {
+                if(word[i] != searchWord[i])
+                {
+                    ++diff;
+                    if(diff > 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (diff == 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+private:
+    vector<string> words;
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */
